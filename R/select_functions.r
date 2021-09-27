965b77b5f0d3806392ddfb6054614ff8c9f1f0ed
Bryan Fuentes <bryandrep@gmail.com>
2021-09-27 15:22:07 -0500

    Fix bug when merging final data frames

--- R/select_functions.r
+++ R/select_functions.r
@@ -298,7 +298,12 @@ select_functions <- function(cu.rast, var.rast, fun = mean,
     }
 
     # Build final table (all variables, all classification units)
-    distfun <- base::Reduce(function(...) base::merge(..., all = T), distlist)
+    distfun <- base::Reduce(function(...) base::merge(...,
+                                                  by = colnames(distlist[[1]]),
+                                                  all = T
+                                                ),
+                          distlist
+                        )
 
     # Return objects (parallel coordinates plot & final table)
     base::list(parcoord = parcoord, distfun = distfun)
