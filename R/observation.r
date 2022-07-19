8baefa89584e3c5d4a9901b33709c5d963b420da
Bryan Fuentes <bryandrep@gmail.com>
2022-07-19 01:17:59 -0500

    Code formatting

--- R/observation.r
+++ R/observation.r
@@ -15,8 +15,8 @@
 #' measured from the observations spatially enclosed by a given stratification
 #' unit. The random selection, as implied by the name, randomly selects one
 #' observation from the set of observations spatially enclosed by a given
-#' stratification unit. See \strong{Details} for some guidance in the use of this
-#' function for classification units.
+#' stratification unit. See \strong{Details} for some guidance in the use of
+#' this function for classification units.
 #'
 #' @param su.rast SpatRaster, as in \code{\link[terra]{rast}}. Single-layer
 #'   SpatRaster representing the stratification units occurring across
@@ -57,8 +57,8 @@
 #' @details
 #' This selection scheme can be applied to classification units. For
 #' classification units, one should replace the multi-layer SpatRaster of
-#' landscape similarities with a multi-layer SpatRaster of spatial signatures. One
-#' should also replace the raster layer of stratification units with that of
+#' landscape similarities with a multi-layer SpatRaster of spatial signatures.
+#' One should also replace the raster layer of stratification units with that of
 #' classification units.
 #'
 #' @examples
@@ -106,32 +106,25 @@ observation <- function(su.rast, obs, col.id, col.resp, method = "mls", ls.rast,
   base::names(su.rast) <- "SU"
 
   # Copy original observations and reduce attributes to just id and response
-  obsSP <- obs[,c(col.id, col.resp)]
+  obsSP <- obs[, c(col.id, col.resp)]
 
   # Extract SUs' numeric codes to each observation
-  obsLS <- terra::extract(su.rast,
-                          obsSP,
-                          method = "simple"
-                        )
+  obsLS <- terra::extract(su.rast, obsSP, method = "simple", na.rm = TRUE)
   obsSP[["SU"]] <- obsLS[, 2]
 
-  # Data table from observations
-  obsDT <- data.table::as.data.table(terra::values(obsSP))
+  # Data frame from observations
+  obsDT <- terra::as.data.frame(obsSP)
 
   # Design based on maximum landscape similarity to SU (mls)
   if(method == "mls" ) {
 
     # Extract SU's landscape similarity values to each observation
-    obsLS <- terra::extract(ls.rast,
-                            obsSP,
-                            method = "simple"
-                          )
+    obsLS <- terra::extract(ls.rast, obsSP, method = "simple", na.rm = TRUE)
 
-    # Merge SU's landscape similarity values with DT from observations
-    obsDAT <- data.table::merge.data.table(obsDT,
-                                           obsLS,
-                                           by = base::colnames(obsDT)[1]
-                                          )
+    # Merge SU's landscape similarity values with DF of observations
+    obsDAT <- base::merge.data.frame(
+      obsDT, obsLS, by = base::colnames(obsDT)[1]
+    )
 
     # Create empty data frame from observations id, response values, and SU
     repval <- obsDAT[0, 1:3]
@@ -149,10 +142,9 @@ observation <- function(su.rast, obs, col.id, col.resp, method = "mls", ls.rast,
 
       # Just to make sure that column name (same as SU's numeric code)...
       # ...for landscape similarity column is numeric
-      sucode <- stringi::stri_replace_all_regex(base::colnames(df)[4],
-                                                "[a-zA-punct]",
-                                                ""
-                                              )
+      sucode <- stringi::stri_replace_all_regex(
+        base::colnames(df)[4], "[a-zA-punct]", ""
+      )
       sucode <- base::as.numeric(sucode)
 
       # Filter df rows that match the SU's numeric code -> Equivalent to...
@@ -171,41 +163,41 @@ observation <- function(su.rast, obs, col.id, col.resp, method = "mls", ls.rast,
 
       # Append rows to data frame with representative observations
       repval <- base::rbind(repval, df)
-      ## Remove NAs
-      repval <- stats::na.omit(repval)
-      ## Sort by ID
-      repval <- repval[base::order(repval[[1]]), ]
-      ## Reset row names
-      base::rownames(repval) <- NULL
-      repval
+
     }
+
+    ## Sort by ID
+    repval <- repval[base::order(repval[[1]]), ]
+    ## Reset row names
+    base::rownames(repval) <- NULL
 
     # Get list of SU without representative observation
     ## Set of SU
-    x <- base::unlist(base::as.list(terra::unique(su.rast)))
+    x <- terra::unique(su.rast)[, 1]
     ## Set of SU with representative observation
-    y <- base::as.numeric(base::as.vector(repval$SU))
+    y <- repval[["SU"]]
     ## Difference between sets
     norepval <- base::setdiff(x, y)
 
     # SpatVector of SUs' representative observations
-    repval.sp <- terra::merge(obs,
-                              repval[, c(1, 3, 4)],
-                              by = base::colnames(repval)[1],
-                              all.x = FALSE
-                            )
+    repval.sp <- terra::merge(
+      obs, repval[, c(1, 3, 4)], by = base::colnames(repval)[1], all.x = FALSE
+    )
 
     # Return objects
-    base::list(su_repobs = repval,
-               su_norepobs = norepval,
-               su_repobs.sp = repval.sp
-              )
+    base::list(
+      su_repobs = repval,
+      su_norepobs = norepval,
+      su_repobs.sp = repval.sp
+    )
 
   } else if(method == "mrv") {
 
     # Function to find observed value closest to the median of all...
     # ...observed values within SU
-    closestval <- function(x) x[base::order(base::abs(x - stats::median(x)))[1]]
+    closestval <- function(x) x[base::order(
+      base::abs(x - stats::median(x))
+    )[1]]
 
     # Column with observed response values
     resp <- base::colnames(obsDT)[2]
@@ -214,10 +206,9 @@ observation <- function(su.rast, obs, col.id, col.resp, method = "mls", ls.rast,
     `%>%` <- dplyr::`%>%`
     repval <- obsDT %>%
       dplyr::group_by(SU) %>%
-        dplyr::mutate_at(dplyr::all_of(resp),
-                         base::list(response = closestval)
-                        )
-    repval <- data.table::as.data.table(repval)
+      dplyr::mutate_at(
+        dplyr::all_of(resp), base::list(response = closestval)
+      )
 
     # Select only rows which corresponding observation has the...
     # ...representative value
@@ -230,57 +221,56 @@ observation <- function(su.rast, obs, col.id, col.resp, method = "mls", ls.rast,
 
     # Get list of SU without representative observation
     ## Set of SU
-    x <- base::unlist(base::as.list(terra::unique(su.rast)))
+    x <- terra::unique(su.rast)[, 1]
     ## Set of SU with representative observation
-    y <- base::as.numeric(base::as.vector(repval$SU))
+    y <- repval[["SU"]]
     ## Difference between sets
     norepval <- base::setdiff(x, y)
 
     # SpatVector of SUs' representative observations
-    repval.sp <- terra::merge(obs,
-                              repval[, c(1,3)],
-                              by = base::colnames(repval)[1],
-                              all.x = FALSE
-                            )
+    repval.sp <- terra::merge(
+      obs, repval[, c(1,3)], by = base::colnames(repval)[1], all.x = FALSE
+    )
 
     # Return objects
-    base::list(su_repobs = repval,
-               su_norepobs = norepval,
-               su_repobs.sp = repval.sp
-              )
+    base::list(
+      su_repobs = repval,
+      su_norepobs = norepval,
+      su_repobs.sp = repval.sp
+    )
 
   } else if(method == "random") {
 
     # Random selection of observation for each SU
     `%>%` <- dplyr::`%>%`
     repval <- obsDT %>% dplyr::group_by(SU) %>% dplyr::sample_n(1)
-    repval <- data.table::as.data.table(repval)
 
     # Get list of SU without representative observation
-    ## Set of SUs
-    x <- base::unlist(base::as.list(terra::unique(su.rast)))
+    ## Set of SU
+    x <- terra::unique(su.rast)[, 1]
     ## Set of SU with representative observation
-    y <- base::as.numeric(base::as.vector(repval$SU))
+    y <- repval[["SU"]]
     ## Difference between sets
     norepval <- base::setdiff(x, y)
 
     # Spatial points data frame of SUs' representative observations
-    repval.sp <- terra::merge(obs,
-                              repval[, c(1,3)],
-                              by = base::colnames(repval)[1],
-                              all.x = FALSE
-                            )
+    repval.sp <- terra::merge(
+      obs, repval[, c(1,3)], by = base::colnames(repval)[1], all.x = FALSE
+    )
 
     # Return objects
-    base::list(su_repobs = repval,
-               su_norepobs = norepval,
-               su_repobs.sp = repval.sp
-              )
+    base::list(
+      su_repobs = repval,
+      su_norepobs = norepval,
+      su_repobs.sp = repval.sp
+    )
 
   } else {
 
     if(verbose == TRUE){
+
       base::warning("Nothing was done. Please select a valid selection method")
+
     }
 
   }
